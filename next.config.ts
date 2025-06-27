f4e5379227452ed418b5c814f66e1d8299c4ec5e
sk <content.type.text.net@gmail.com>
2025-06-27 19:02:26 +0900

    add npm-check-updates

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-	enabled: process.env.ANALYZE === "true",
-});
 
 const nextConfig: NextConfig = {
 	output: "export",
