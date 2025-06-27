ac43344176a64161a04d8c2146b9c44de6692be0
sk <content.type.text.net@gmail.com>
2025-06-27 19:02:19 +0900

    add npm-check-updates

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,16 @@
 import type { NextConfig } from "next";
 
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+	enabled: process.env.ANALYZE === "true",
+});
 
 const nextConfig: NextConfig = {
 	output: "export",
 	compiler: {
-		removeConsole: process.env.NODE_ENV === "production"
-		? { exclude: ["error", "warn", "info"] }
-		: false,
+		removeConsole:
+			process.env.NODE_ENV === "production"
+				? { exclude: ["info", "warn", "error"] }
+				: false,
 	}
 };
 
