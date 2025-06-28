da5d408bd90aa8a8b3cdced90fb03a916fb5a31f
sk <content.type.text.net@gmail.com>
2025-06-28 17:59:47 +0900

    コピペでなんも理解してないけど地図っぽい

---
+++ components/map/GlobalMap.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import Leaflet from "leaflet";
+import { FC } from "react";
+import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import "./GlobalMap.css";
+
+Leaflet.Icon.Default.imagePath =
+	"//cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/";
+
+export interface GlobalMapProps {
+	test?: number;
+}
+
+export const GlobalMap: FC<GlobalMapProps> = (props) => {
+	return (
+		<MapContainer
+			center={[51.505, -0.09]}
+			zoom={13}
+			scrollWheelZoom={false}
+		>
+			<TileLayer
+				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+				url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+			/>
+			<Marker position={[51.505, -0.09]}>
+				<Popup>
+					A pretty CSS3 popup. <br /> Easily customizable.
+				</Popup>
+			</Marker>
+		</MapContainer>
+	);
+};
+
+export default GlobalMap;
--- pages/map.tsx
+++ pages/map.tsx
@@ -1,8 +1,24 @@
 import type { NextPage } from "next";
 import { DefaultPage } from "@/components/layout/DefaultPage";
+import "leaflet/dist/leaflet.css";
+import dynamic from "next/dynamic";
+import { useMemo } from "react";
 
 const Page: NextPage = () => {
-	return <DefaultPage pageId="map">a</DefaultPage>;
+	const GlobalMap = useMemo(
+		() =>
+			dynamic(() => import("@/components/map/GlobalMap"), {
+				loading: () => <p>A map is loading</p>,
+				ssr: false,
+			}),
+		[],
+	);
+
+	return (
+		<DefaultPage pageId="map">
+			<GlobalMap />
+		</DefaultPage>
+	);
 };
 
 export default Page;
